ad903f0bf1717d9701e886197735a47fe6911b5e
Pierrick Rassat <rassat.pierrick@gmail.com>
2018-07-21 17:41:18 +0200

    Force jekyll auto reload for windows

--- create_jekyll_server.bat
+++ create_jekyll_server.bat
@@ -1,2 +1,2 @@
-docker run -d -v C:/dev/code/github/kraiss.github.io/:/srv/jekyll -p 80:4000 --name=jekyll jekyll/jekyll jekyll serve
+docker run -d -v C:/dev/code/github/kraiss.github.io/:/srv/jekyll -p 80:4000 --name=jekyll jekyll/jekyll jekyll serve -w --force_polling
 docker logs -f jekyll
